01e48780187b9caae5562768b6c2ab6f8568fe0c
adhilmulavoor <adhilmulavoor@gmail.com>
2026-03-07 09:13:56 +0530

    update the background color of the login pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} min-h-screen flex`}>
         <Navigation />
-        <main className="flex-1 flex flex-col min-h-screen min-w-0 bg-[#0f172a]">
+        <main className="flex-1 flex flex-col min-h-screen min-w-0">
           <div className="flex-1 w-full min-w-0">
             {children}
           </div>
